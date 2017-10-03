31906820547df7c81cc9cf73e74824ede816dc58
Andy Wooller <andy.wooller@weclick.media>
2017-10-03 11:38:35 +0100

    wcm boilerplate init

---
+++ DigitalVisitorGuide/android/app/src/main/java/com/digitalvisitorguide/MainApplication.java
@@ -0,0 +1,42 @@
+package com.digitalvisitorguide;
+
+import android.app.Application;
+
+import com.reactnativenavigation.NavigationApplication;
+
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+
+import java.util.Arrays;
+import java.util.List;
+import com.crashlytics.android.Crashlytics; 
+import io.fabric.sdk.android.Fabric;
+
+public class MainApplication extends NavigationApplication {
+@Override
+   public void onCreate() {
+       super.onCreate();
+       Fabric.with(this, new Crashlytics());
+   }
+
+  @Override
+  public boolean isDebug() {
+     // Make sure you are using BuildConfig from your own application
+     return BuildConfig.DEBUG;
+  }
+
+  protected List<ReactPackage> getPackages() {
+     // Add additional packages you require here
+     // No need to add RnnPackage and MainReactPackage
+     return Arrays.<ReactPackage>asList(
+         // eg. new VectorIconsPackage()
+     );
+  }
+
+  @Override
+  public List<ReactPackage> createAdditionalReactPackages() {
+     return getPackages();
+  }
+}
