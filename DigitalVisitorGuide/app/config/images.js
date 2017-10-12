b0db071daf73c204ee9acf1fedf451abce3441f5
Andy Wooller <andy.wooller@weclick.media>
2017-10-12 15:25:27 +0100

    images

--- DigitalVisitorGuide/app/config/images.js
+++ DigitalVisitorGuide/app/config/images.js
@@ -6,7 +6,13 @@
 const images = {
     welcomeScreenImage: require('../images/welcome_background_image.jpg'),
     towerOfLondonLogo: require('../images/tower_of_london_logo.png'),
-    welcomeAccordionImage: require('../images/welcome_accordion_image.jpg')
+    welcomeAccordionImage: require('../images/welcome_accordion_image.jpg'),
+    thomasAccordionImage: require('../images/thomas_accordion_image.jpg'),
+    medievalAccordionImage: require('../images/medieval_accordion_image.jpg'),
+    normansAccordionImage: require('../images/normans_accordion_image.jpg'),
+    imprisonmentAccordionImage: require('../images/imprisonment_accordion_image.jpg'),
+    crownAccordionImage: require('../images/crown_accordion_image.jpg'),
+    towerAccordionImage: require('../images/tower_accordion_image.jpg'),
 };
 
 export default images;
