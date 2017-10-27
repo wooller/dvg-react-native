89e55e66042569371f96e96c31fb8afcbd7ff9ad
Andy Wooller <andy.wooller@weclick.media>
2017-10-27 13:17:27 +0100

    navigate to explore on tap

--- DigitalVisitorGuide/app/routes/primarySideMenu/primarySideMenu.js
+++ DigitalVisitorGuide/app/routes/primarySideMenu/primarySideMenu.js
@@ -4,7 +4,7 @@
  * where is it called from
  */
 
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { Text, View, Image, ScrollView, TouchableNativeFeedback } from 'react-native';
 import styles from './styles';
 import images from '../../config/images';
@@ -12,37 +12,37 @@ import textStyles from '../../config/textStyles';
 import {globalStyles} from '../../config/styles';
 import Icon from '../../config/customIcons';
 
-const PrimarySideMenu = (props) => {
-    return (
-        <ScrollView scrollsToTop={false} style={styles.container}>
-            <TouchableNativeFeedback  onPress={() => this.props.onItemSelected('Explore')} >
+export default class PrimarySideMenu extends PureComponent {
+    constructor(props) {
+        super(props);
+    }
+    render(){
+        return (
+            <ScrollView scrollsToTop={false} style={styles.container}>
+                <TouchableNativeFeedback  onPress={() => {this.props.navigator.toggleDrawer({side: 'right', animated: true}); this.props.navigator.push({screen: 'digitalVisitorGuide.exploreScreen', animationType: globalStyles.navigationAnimation})}}>
+                    <View style={styles.menuItem}>
+                        <Icon name="explore" style={styles.menuIcon} size={20} color={globalStyles.darkGrey} />
+                        <Text style={[textStyles.menuItemText, styles.textItem]}>Explore</Text>
+                    </View>
+                </TouchableNativeFeedback>
                 <View style={styles.menuItem}>
-                    <Icon name="explore" style={styles.menuIcon} size={20} color={globalStyles.darkGrey} />
-                    <Text style={[textStyles.menuItemText, styles.textItem]}>Explore</Text>
+                    <Icon name="plan" style={styles.menuIcon} size={20} color={globalStyles.darkGrey} />
+                    <Text style={[textStyles.menuItemText, styles.textItem]}>Plan your day</Text>
                 </View>
-            </TouchableNativeFeedback>
-            <View style={styles.menuItem}>
-                <Icon name="plan" style={styles.menuIcon} size={20} color={globalStyles.darkGrey} />
-                <Text style={[textStyles.menuItemText, styles.textItem]}>Plan your day</Text>
-            </View>
-            <View style={styles.menuItem}>
-                <Icon name="cafe" style={styles.menuIcon} size={15} color={globalStyles.darkGrey} />
-                <Text style={[textStyles.menuItemText, styles.textItem]}>Food and Drink</Text>
-            </View>
-            <View style={styles.menuItem}>
-                <Icon name="shops" style={styles.menuIcon} size={20} color={globalStyles.darkGrey} />
-                <Text style={[textStyles.menuItemText, styles.textItem]}>Shops</Text>
-            </View>
-            <View style={styles.menuItem}>
-                <Icon name="settings" style={styles.menuIcon} size={20} color={globalStyles.darkGrey} />
-                <Text style={[textStyles.menuItemText, styles.textItem]}>Change language</Text>
-            </View>
-        </ScrollView>
-    );
-};
-
-PrimarySideMenu.propTypes = {
+                <View style={styles.menuItem}>
+                    <Icon name="cafe" style={styles.menuIcon} size={15} color={globalStyles.darkGrey} />
+                    <Text style={[textStyles.menuItemText, styles.textItem]}>Food and Drink</Text>
+                </View>
+                <View style={styles.menuItem}>
+                    <Icon name="shops" style={styles.menuIcon} size={20} color={globalStyles.darkGrey} />
+                    <Text style={[textStyles.menuItemText, styles.textItem]}>Shops</Text>
+                </View>
+                <View style={styles.menuItem}>
+                    <Icon name="settings" style={styles.menuIcon} size={20} color={globalStyles.darkGrey} />
+                    <Text style={[textStyles.menuItemText, styles.textItem]}>Change language</Text>
+                </View>
+            </ScrollView>
+        );
+    }
 
 };
-
-export default PrimarySideMenu;
